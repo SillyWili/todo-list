0aa6498f05838a8d4a04e8aac697bb1c0474a09f
Wiliam Muller <174868022+SillyWili@users.noreply.github.com>
2025-03-06 21:29:31 +0200

    fix: list as an array instead of object

--- src/logic/todoManager.js
+++ src/logic/todoManager.js
@@ -2,19 +2,20 @@ import { TodoList } from "./todo.js";
 
 export class TodosManager {
   constructor() {
-    this.list = {};
+    this.list = [];
     this.createNewList("Default");
   }
 
   createNewList(name) {
-    if (!this.list[name]) {
-      this.list[name] = new TodoList(name);
+    if (!this.list.find((item) => item.name === name)) {
+      const newList = new TodoList(name);
+      this.list.push(newList);
     }
-    return this.list[name];
+    return this.getSpecificList(name);
   }
 
   getSpecificList(name) {
-    return this.list[name];
+    return this.list.find((item) => item.name === name);
   }
 
   getAllList() {
